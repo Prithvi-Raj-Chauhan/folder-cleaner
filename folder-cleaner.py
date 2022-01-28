1d86e2df9b8bde2486607dd7f04e256e230ff3de
Divyanshu Padhy <divyadollypadhy@outlook.com>
2022-01-28 11:23:13 -0800

    initial setup

---
+++ folder-cleaner.py
@@ -0,0 +1,10 @@
+# Basic modules
+import os # for listing files and making folders
+import json # for reading structure.json
+
+STRUCUTRE = json.load(
+    open('structure.json')
+)
+
+if __name__ == "__main__":
+    pass
